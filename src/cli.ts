8741ad59ca4ada8a1287110867e5daf58d2fdabd
Railly Hugo <51397083+Railly@users.noreply.github.com>
2026-08-22 00:16:52 -0300

    chore(release): publish cuse-cli 0.2.1 (#87)

--- src/cli.ts
+++ src/cli.ts
@@ -1184,7 +1184,7 @@ async function act(action: string, args: string[], opts: Options = {}): Promise<
   }
 }
 
-export const VERSION = "0.2.0";
+export const VERSION = "0.2.1";
 
 /**
  * One process, many commands.
